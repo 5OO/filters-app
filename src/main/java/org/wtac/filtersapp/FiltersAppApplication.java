873946ba03d085f9152259f6a57798cddf0a8d9e
5OO <git.hub@myrk.eu>
2024-03-31 14:25:38 +0300

    Initial commit

---
+++ src/main/java/org/wtac/filtersapp/FiltersAppApplication.java
@@ -0,0 +1,13 @@
+package org.wtac.filtersapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiltersAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiltersAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/wtac/filtersapp/FiltersAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.wtac.filtersapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiltersAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
